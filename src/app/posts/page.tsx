32a01876b97d748ed480efffe6df023f1d77d697
gary wallace <garyatgreatvacation4u@gmail.com>
2024-10-24 17:42:58 -0400

    removed resolveMetadat=> to posts && added static metadat

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import Index from "@/components/posts/Index";
 import { Metadata, ResolvingMetadata } from 'next';
 import Meta from '@/components/meta/meta';
-//genposts_metadata
+export const metadata = Meta.metaPosts();
 
 type Props = {
     params: { id: string }
     searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
-    const metaBlogs = await Meta.genposts_metadata(parent);
-    return metaBlogs;
+// export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+//     const metaBlogs = await Meta.genposts_metadata(parent);
+//     return metaBlogs;
 
-}
+// }
 
 export default function page() {
     return (
