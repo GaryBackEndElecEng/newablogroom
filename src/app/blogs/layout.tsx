e7a6ee83d05232b14fd92f5afeb1f3bcabf71883
gary wallace <garyatgreatvacation4u@gmail.com>
2024-10-29 11:03:51 -0400

    removed non async function as test

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -1,48 +1,48 @@
-"use server";
+"use server";//USE SERVER CAN ONLY EXPORT ASYNC FUNCTION
 import React from 'react';
-import type { Metadata } from 'next';
+import type { ResolvingMetadata } from 'next';
 // export const metadata: Metadata = meta.metaBlogs();
 // const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXTAUTH_URL as string : process.env.DOMAIN as string;
 
-export const metadata: Metadata = {
-    title: {
-        default: "blogs",
-        template: `%s | blogs`,
+// export const metadata: Metadata = {
+//     title: {
+//         default: "blogs",
+//         template: `%s | blogs`,
 
-    },
-    description: "Comment page",
-    keywords: ["comments and messages", "comments", "ask Us something", "helping you connect", " message board"],
+//     },
+//     description: "Comment page",
+//     keywords: ["comments and messages", "comments", "ask Us something", "helping you connect", " message board"],
 
-    alternates: {
-        canonical: "/blogs",
-        languages: {
-            "en-US": "/en-US",
-            "fr-CA": "/fr-CA"
-        }
-    },
-    openGraph: {
-        title: "Blogs",
-        description: 'blogs',
-        url: "/blogs",
-        images: [
-            {
-                url: "/images/gb_logo.png",
-                width: 300,
-                height: 300
-            },
-            {
-                url: "/images/display/symetric.png",
-                width: 400,
-                height: 300
-            },
-            {
-                url: "https://new-master.s3.ca-central-1.amazonaws.com/static/masterultils/logoLarge.png",
-                width: 600,
-                height: 900
-            },
-        ],
-    }
-}
+//     alternates: {
+//         canonical: "/blogs",
+//         languages: {
+//             "en-US": "/en-US",
+//             "fr-CA": "/fr-CA"
+//         }
+//     },
+//     openGraph: {
+//         title: "Blogs",
+//         description: 'blogs',
+//         url: "/blogs",
+//         images: [
+//             {
+//                 url: "/images/gb_logo.png",
+//                 width: 300,
+//                 height: 300
+//             },
+//             {
+//                 url: "/images/display/symetric.png",
+//                 width: 400,
+//                 height: 300
+//             },
+//             {
+//                 url: "https://new-master.s3.ca-central-1.amazonaws.com/static/masterultils/logoLarge.png",
+//                 width: 600,
+//                 height: 900
+//             },
+//         ],
+//     }
+// }
 
 type Props = {
     params: { id: string }
